616de8d271b7bf4525db0c7b6481903115ee5a92
Stev3nT <steven.tsvetkov@gmail.com>
2015-06-27 13:13:02 +0300

    Updating front-end dependencies && switching to .min files

--- InfinityBlog.Web/App_Start/BundleConfig.cs
+++ InfinityBlog.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace InfinityBlog.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,12 +20,12 @@ namespace InfinityBlog.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/styles.min.css"));
         }
     }
 }
