76ebac9ed1248bd6c4d0c7b5f9cde595515d068d
Stev3nT <steven.tsvetkov@gmail.com>
2015-06-27 12:52:51 +0300

    Adding Web Project && Unit Tests

---
+++ InfinityBlog.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InfinityBlog.Web.Startup))]
+namespace InfinityBlog.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
