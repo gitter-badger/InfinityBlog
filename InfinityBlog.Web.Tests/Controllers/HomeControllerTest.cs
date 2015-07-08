2217a671c128c90380239d3126f5790830d5b52b
Steven Tsvetkov <steven.tsvetkov@gmail.com>
2015-07-08 13:17:27 +0300

    Update HomeControllerTest.cs

--- InfinityBlog.Web.Tests/Controllers/HomeControllerTest.cs
+++ InfinityBlog.Web.Tests/Controllers/HomeControllerTest.cs
@@ -9,19 +9,19 @@ namespace InfinityBlog.Web.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
-        [TestMethod]
-        public void Index()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
+        //[TestMethod]
+        //public void Index()
+        //{
+        //    // Arrange
+        //    HomeController controller = new HomeController();
+        //
+        //    // Act
+        //    ViewResult result = controller.Index() as ViewResult;
+        //
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //}
+        //
         //[TestMethod]
         //public void About()
         //{
